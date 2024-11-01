1526a33d9625e70387c7e7dc81609d46c711b55f
ULC-Panoramics <robert.walter@unitedliving.co.uk>
2024-11-01 11:45:28 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -44,7 +44,7 @@ var viewer = pannellum.viewer('panorama', {
 
       "image-4":{
         "type": "equirectangular",
-         "panorama": "NA2",
+         "panorama": "NA",
          "hfov":200,
          "haov": 360,
          "vaov":150,
